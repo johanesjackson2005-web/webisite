a918f77544a5636db1698fbe6f64b3ea2b4347d4
Johanes Jackson <johanesjackson2005@gmail.com>
2026-05-25 15:04:07 -0700

    Updated requirements

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9l2(=n2x0*ar52za5^q0#)7s^8a9%dpegml65uz*4l(^-p#6oc'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
