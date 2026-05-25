1b942287f98505d136a3f4bf83d99b393f4ff171
Johanes Jackson <johanesjackson2005@gmail.com>
2026-05-25 16:30:48 -0700

    change

--- myproject/settings.py
+++ myproject/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-env = environ.Env()
-
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
